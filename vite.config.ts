5e597fbbbb9b1249607342bc368a39587c266056
JoyM1K1 <kohchanman14@gmail.com>
2025-03-21 12:54:40 +0900

    🐛 apply deadcode removal

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,10 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: "node",
-    includeSource: ["**/*.{js,ts}"],
+    environment: 'node',
+    includeSource: ['**/*.{js,ts}'],
+  },
+  define: {
+    'import.meta.vitest': 'undefined',
   },
 });
